83417503cc871b6dd80060f76ae10766e0f45665
brennanjleahey <brennanjleahey@gmail.com>
2020-03-08 16:55:25 -0700

    Fixed navigation form error.

--- cougar_bulletin/lib/main.dart
+++ cougar_bulletin/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           theme: new ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: new LoginPage(auth: new Auth())
+          home: new RootPage(auth: new Auth())
         );
   }
 }
