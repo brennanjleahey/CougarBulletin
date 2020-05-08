66b9bad1b8eafae8024d25e9c89250a055838f74
brennanjleahey <brennanjleahey@gmail.com>
2020-05-08 00:02:13 -0700

    Developing My Profile feature

--- cougar_bulletin/lib/myposts_page.dart
+++ cougar_bulletin/lib/myposts_page.dart
@@ -1,4 +1,10 @@
+import 'package:cougar_bulletin/notifier/auth_notifier.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'api/post_api.dart';
+import 'notifier/post_notifier.dart';
 
 class MyPosts extends StatefulWidget {
   
@@ -9,12 +15,18 @@ class MyPosts extends StatefulWidget {
 
 class _MyPostsState extends State<MyPosts> {
 
+
+@override
+  void initState() {
+    PostNotifier postNotifier = Provider.of<PostNotifier>(context, listen: false);
+    getPosts(postNotifier);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
+    AuthNotifier authNotifier = Provider.of<AuthNotifier>(context);
 
-    
-    // TODO: implement build
-    return null;
+    return Scaffold();
   }
 
 
