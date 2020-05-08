6653729d0100a4189f43d8d84968830f88fd0d9c
brennanjleahey <brennanjleahey@gmail.com>
2020-05-07 23:57:02 -0700

    Adding My Posts page

---
+++ cougar_bulletin/lib/myposts_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class MyPosts extends StatefulWidget {
+  
+  @override
+  _MyPostsState createState() => _MyPostsState();
+
+}
+
+class _MyPostsState extends State<MyPosts> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    
+    // TODO: implement build
+    return null;
+  }
+
+
+}
